40d7b9822196afc1a13c10fcf29770332a431684
Francisco Andrade <titoandrade00@gmail.com>
2023-06-10 19:42:03 +0100

    Layout paginas fixed -> vou melhorar ainda mais

--- reactnative/BraGuiaReact/screens/login.js
+++ reactnative/BraGuiaReact/screens/login.js
@@ -3,18 +3,28 @@ import {StyleSheet, View,Text,TextInput,Image,Button} from 'react-native';
 
 export default function LoginScreen() {
   return (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  <View style={styles.container}>
+       {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> */}
+       <Text style={styles.title}>Login</Text>
+       <View style={styles.logoContainer}>       
             <Image
               source={require('../assets/images/login.png')}
               style={styles.image_icon}
             /> 
-        </View>    
-    <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 10 }}>Login</Text>
-    <Text style={{ fontSize: 22, fontWeight: 'bold', marginTop: 36 }}>User</Text>
-    <TextInput style={{ width: 325, height: 40 }} placeholder="Enter username" />
-    <Text style={{ fontSize: 22, fontWeight: 'bold', marginTop: 16 }}>Password</Text>
-    <TextInput style={{ width: 325, height: 40 }} secureTextEntry placeholder="Enter password" />
+        </View>   
+    <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+        />
+    </View>
+    {/* <Text style={{ fontSize: 22, fontWeight: 'bold', marginTop: 36 }}>User</Text> */}    
     <Button title="Go" onPress={() => {}} />
   </View>
   );
@@ -23,10 +33,33 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 40,
   },
   image_icon:{
     width: 100,
     height: 100,
     resizeMode: 'contain',
-  }
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 10,
+  },
 });
--- reactnative/BraGuiaReact/screens/register.js
+++ reactnative/BraGuiaReact/screens/register.js
@@ -4,36 +4,76 @@ import {StyleSheet, View,Text,TextInput,Image,Button} from 'react-native';
 
 export default function RegisterScreen() {
   return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image
-              source={require('../assets/images/register.png')}
-              style={styles.image_icon}
-            /> 
-        </View> 
-        <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 10 }}>Register</Text>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 36 }}>User</Text>
-        <TextInput style={{ width: 299, height: 48 }} placeholder="Enter username" />
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 16 }}>First Name</Text>
-        <TextInput style={{ width: 299, height: 48 }} placeholder="Enter first name" />
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 16 }}>Last Name</Text>
-        <TextInput style={{ width: 299, height: 48 }} placeholder="Enter last name" />
-        <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 16 }}>Email</Text>
-        <TextInput style={{ width: 304, height: 48 }} placeholder="Enter email" keyboardType="email-address" />
-        <Text style={{ fontSize: 22, fontWeight: 'bold', marginTop: 16 }}>Password</Text>
-        <TextInput style={{ width: 325, height: 48 }} secureTextEntry placeholder="Enter password" />
-        <Button title="Register" onPress={() => {}} />
+        <View style={styles.container}>
+          <Text style={styles.title}>Register</Text>
+        {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> */}
+          <View style={styles.logoContainer}>       
+              <Image
+                source={require('../assets/images/register.png')}
+                style={styles.image_icon}
+              /> 
+          </View>    
+          {/* <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 10 }}>Register</Text> */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Name"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Surname"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+            />
+          </View>
+        {/* <Text style={{ fontSize: 22, fontWeight: 'bold', marginTop: 36 }}>User</Text> */}    
+        <Button title="Go" onPress={() => {}} />
       </View>
-  );
+);
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 40,
   },
   image_icon:{
     width: 100,
     height: 100,
     resizeMode: 'contain',
-  }
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 10,
+  },
 });
