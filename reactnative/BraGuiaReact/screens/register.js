74d019ae6c17b8536e7cccd8074c5320ca8f6888
Francisco Andrade <titoandrade00@gmail.com>
2023-06-10 18:48:15 +0100

    Vamos la ver se é desta

--- reactnative/BraGuiaReact/screens/register.js
+++ reactnative/BraGuiaReact/screens/register.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {StyleSheet, View,Text,TextInput,Button} from 'react-native';
+import {StyleSheet, View,Text,TextInput,Image,Button} from 'react-native';
 
 
 export default function RegisterScreen() {
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Image
+              source={require('../assets/images/register.png')}
+              style={styles.image_icon}
+            /> 
+        </View> 
         <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 10 }}>Register</Text>
         <Text style={{ fontSize: 20, fontWeight: 'bold', marginTop: 36 }}>User</Text>
         <TextInput style={{ width: 299, height: 48 }} placeholder="Enter username" />
@@ -24,5 +30,10 @@ export default function RegisterScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  image_icon:{
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
   }
 });
