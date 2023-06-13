8d45f50aa8ef19f70acf9e293cbe0e3d4834617b
goncalom55 <goncalormedeiros@gmail.com>
2023-06-13 14:52:22 +0100

    redux init

--- reactnative/BraGuiaReact/App.tsx
+++ reactnative/BraGuiaReact/App.tsx
@@ -22,12 +22,17 @@ import RegisterScreen from './screens/register';
 import FirstPage from './screens/firstpage';
 import Navigator from './routes/homeStack';
 
+import { Provider } from "react-redux";
+import store from './redux/store';
 
 //const MainActivity = ({ navigation }) => {
   export default function App(){
     //const [name, setName] = useState('main');
     return (
-      <Navigator />
+      <Provider store={store}>
+        <Navigator />
+      </Provider>
+      
     );
   };
   
@@ -36,104 +41,6 @@ import Navigator from './routes/homeStack';
       flex: 1,
     }
   });
-
-/*function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}*/
-
-//function App(): JSX.Element {
-  /*const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}*/
-
-/*const AppNavigator = createStackNavigator(
-  {
-    Home: {
-      screen: MainActivity,
-      navigationOptions: ({ navigation }) => ({
-        title: 'Your App Title',
-        headerRight: (
-          <HeaderButtons>
-            <Item
-              title="Add"
-              iconName="add"
-              onPress={() => {
-                // Handle your action here
-              }}
-            />
-          </HeaderButtons>
-        ),
-      }),
-    },
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: '#ffffff', // Customize your header background color
-      },
-      headerTintColor: '#000000', // Customize your header text color
-    },
-  }
-);*/
-
 
 
 
